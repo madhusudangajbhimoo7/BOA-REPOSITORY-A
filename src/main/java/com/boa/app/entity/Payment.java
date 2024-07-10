12906673166e4edd529c0b4b0adb34bd1d6e0988
hithisispalak1 <palakvalecha71@gmail.com>
2024-07-10 14:25:49 +0530

    commit to update Payment

--- src/main/java/com/boa/app/entity/Payment.java
+++ src/main/java/com/boa/app/entity/Payment.java
@@ -1,5 +1,69 @@
 package com.boa.app.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Payment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private Long userId;
+	
+	private BigDecimal amount;
+	
+	private String status;
+	
+	private LocalDateTime createdAt;
+	
+	
+	//Getters and Setters
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public BigDecimal getAmount() {
+		return amount;
+	}
+
+	public void setAmount(BigDecimal amount) {
+		this.amount = amount;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+	
+	
 
 }
--- src/main/java/com/boa/app/repo/PaymentRepo.java
+++ src/main/java/com/boa/app/repo/PaymentRepo.java
@@ -1,5 +1,9 @@
 package com.boa.app.repo;
 
-public interface PaymentRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.boa.app.entity.Payment;
+
+public interface PaymentRepo extends JpaRepository<Payment, Long>{
 
 }
